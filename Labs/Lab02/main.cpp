318dbd252061abcbac78a2842aea1713a8d78b65
RootCellar <RootCellar9877@GMail.com>
2021-01-21 10:43:20 -0900

    Complete lab 02

--- Labs/Lab02/main.cpp
+++ Labs/Lab02/main.cpp
@@ -1,22 +1,65 @@
 #include "other.h"
 
+//Pass by value
 void doSomething(Foo f) {
   //copied
   //This will show copy constructor, then destructor
+}
+
+//Passed the reference!
+void doSomethingElse(Foo &f) {
+  f.setData(7);
+}
+
+//Passed by reference to const
+void doAnotherThing(const Foo &f) {
+  //f.setData(8);
+}
+
+Foo returnAFoo() {
+  Foo foo(61);
+  return foo;
 }
 
 int main() {
 
   //Make a foo
   Foo f;
+  f.setData(2);
 
   //Make a foo and destroy it
   {
     Foo d;
   }
 
+  //Copy f to bar
+  Foo bar = f;
+  bar.setData(3);
+
+  //Make a foo with some data
+  Foo dataFoo(6);
+
   //Copy a foo
   doSomething(f);
+
+  doSomethingElse(dataFoo);
+
+  doAnotherThing(dataFoo);
+
+  returnAFoo();
+  cout << endl << endl;
+
+  cout << "Time for vector stuff" << endl << endl;
+  //Vector stuff
+  vector<Foo> fooList;
+
+  for(int i=25; i<30; i++) {
+    Foo theFoo(i);
+    fooList.push_back(theFoo);
+  }
+
+  cout << endl << endl;
+  cout << "Program done, everything will now be destroyed" << endl;
 
   return 1;
 }
--- Labs/Lab02/other.h
+++ Labs/Lab02/other.h
@@ -9,10 +9,19 @@ using std::endl;
 using std::vector;
 
 class Foo {
+private:
+
+  int _data;
+  bool _isSet = false;
+
 public:
   Foo();
   ~Foo();
   Foo(const Foo& original);
+  Foo(int x);
+
+  void setData(int x);
+  int getData() const;
 };
 
 
