54a2dda72529dd00e05e0790d61dc82ebcc2ac4a
RootCellar <RootCellar9877@GMail.com>
2021-01-31 20:30:33 -0900

    implement reading for other 2 data containers

--- HW/HW1/TimeIt2/main.cpp
+++ HW/HW1/TimeIt2/main.cpp
@@ -11,6 +11,10 @@ using std::forward_list;
 using std::ifstream;
 
 int main() {
+  //READING
+
+  //VECTOR
+
   ifstream input(FILE_NAME);
 
   if(!input) {
@@ -42,5 +46,59 @@ int main() {
   watch.stop();
 
   cout << "Vector Read: " << watch.getElapsedSeconds() << " seconds (" << data1.size() << " strings)"<< endl;
+
+  //LIST
+
+  ifstream input2(FILE_NAME);
+
+  if(!input2) {
+    cout << "ERROR ON OPENING FILE" << endl;
+    return -1;
+  }
+
+  watch.start();
+  while(true) {
+    input2 >> data;
+    if(!input2) {
+      if(input2.eof()) {
+        cout << "Reached end of file, done with read..." << endl;
+        break;
+      }
+      cout << "ERROR ON READ" << endl;
+      return -2;
+    }
+
+    data2.push_back(data);
+  }
+  watch.stop();
+
+  cout << "List Read: " << watch.getElapsedSeconds() << " seconds (" << data2.size() << " strings)"<< endl;
+
+  //FORWARD LIST
+
+  ifstream input3(FILE_NAME);
+
+  if(!input3) {
+    cout << "ERROR ON OPENING FILE" << endl;
+    return -1;
+  }
+
+  watch.start();
+  while(true) {
+    input3 >> data;
+    if(!input3) {
+      if(input3.eof()) {
+        cout << "Reached end of file, done with read..." << endl;
+        break;
+      }
+      cout << "ERROR ON READ" << endl;
+      return -2;
+    }
+
+    data3.push_front(data);
+  }
+  watch.stop();
+
+  cout << "Forward List Read: " << watch.getElapsedSeconds() << endl;
 
 }
