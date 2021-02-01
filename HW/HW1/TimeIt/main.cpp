dada5088a88db081d004facdcca534282ad6eda2
RootCellar <RootCellar9877@GMail.com>
2021-01-31 18:37:34 -0900

    setup main()

--- HW/HW1/TimeIt/main.cpp
+++ HW/HW1/TimeIt/main.cpp
@@ -5,9 +5,17 @@
  * Using the stopwatch class to test algorithms
 */
 
+#define DATA_SIZE 10
+
 
 #include "stopwatch.h"
 
 int main() {
+  random_device dev;
+  mt19937 random(dev());
+
+  uniform_int_distribution<> distribution(1, 1000);
+
+  
 
 }
