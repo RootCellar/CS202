3f01d7fd968fd5288b04b880f91e36b5b6053ff2
RootCellar <RootCellar9877@GMail.com>
2021-01-31 18:41:54 -0900

    implement makeData()

--- HW/HW1/TimeIt/main.cpp
+++ HW/HW1/TimeIt/main.cpp
@@ -10,12 +10,22 @@
 
 #include "stopwatch.h"
 
+vector<int> makeData(mt19937 &rand, uniform_int_distribution<> &dis) {
+  vector<int> data;
+
+  for(int i=0; i < DATA_SIZE; i++) {
+    data.push_back( dis(rand) );
+  }
+
+  return data;
+}
+
 int main() {
   random_device dev;
   mt19937 random(dev());
 
   uniform_int_distribution<> distribution(1, 1000);
 
-  
+
 
 }
