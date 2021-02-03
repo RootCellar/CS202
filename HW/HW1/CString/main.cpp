49a0e68ccaaf3071c8a0528655bd72393b3c933c
RootCellar <RootCellar9877@GMail.com>
2021-02-03 10:45:07 -0900

    Comments, optimizations, remove debug

--- HW/HW1/CString/main.cpp
+++ HW/HW1/CString/main.cpp
@@ -6,22 +6,28 @@ using std::endl;
 
 using std::string;
 
-#define DEBUG 1
+//#define DEBUG 1
 
+//Duplicate from into dynamically allocated memory
 char* strdup(const char* from) {
+  //Find Size
   int size = 0;
   while(from[size] != '\0') ++size;
   ++size;
 
+  //Make a new char*
   char* toRet = new char[size];
 
+  //Copy
   for(int i=0; i<size; i++) {
     toRet[i] = from[i];
   }
 
+  //Return!
   return toRet;
 }
 
+//Find x in s
 const char* findx(const char* s, const char* x) {
   //Forgetting to set these initial values resulted in segmentation fault...whoops!
   int sizeS = 0;
@@ -30,6 +36,8 @@ const char* findx(const char* s, const char* x) {
   #ifdef DEBUG
   cout << "Here1" << endl;
   #endif
+
+  //Find sizes
 
   while(s[sizeS] != '\0') ++sizeS;
   ++sizeS;
@@ -41,8 +49,9 @@ const char* findx(const char* s, const char* x) {
   cout << "Here2" << endl;
   #endif
 
+  //Find x in s!
   bool found;
-  for(int i=0; i < sizeS; i++) {
+  for(int i=0; i < sizeS; ++i) {
     found = true;
 
     #ifdef DEBUG
@@ -51,7 +60,7 @@ const char* findx(const char* s, const char* x) {
 
     if(s[i] == x[0]) {
 
-      for(int j=0; j+i < sizeS && j < sizeX-1; j++) {
+      for(int j=0; j+i < sizeS && j < sizeX-1; ++j) {
 
         if(s[i+j] != x[j]) {
           found = false;
