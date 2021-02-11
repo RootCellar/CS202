39607798920a9e13f68d36a1854e4c842164f1bd
RootCellar <RootCellar9877@GMail.com>
2021-02-10 19:32:51 -0900

    implement printPointer()

---
+++ HW/HW2/Memory/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * Darian Marvel
+ * 2/10/2021
+ * main.cpp
+ * Checking out how the compiler lays out memory
+*/
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void printPointer(int& i) {
+  cout << "i: " << i << endl;
+  cout << "&i: " << &i << endl;
+}
+
+int main() {
+  
+}
