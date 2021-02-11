802f04d242e6b0f9b64f414fb6b95e216cbb7fe3
RootCellar <RootCellar9877@GMail.com>
2021-02-10 19:33:02 -0900

    finish main

--- HW/HW2/Memory/main.cpp
+++ HW/HW2/Memory/main.cpp
@@ -15,6 +15,30 @@ void printPointer(int& i) {
   cout << "&i: " << &i << endl;
 }
 
+static int a = 1;
+static int b = 2;
+
 int main() {
-  
+  cout << "----" << " Static Data " << "----" << endl;
+  printPointer(a);
+  printPointer(b);
+
+  cout << "----" << " Stack" << "----" << endl;
+  int x = 3;
+  int y = 4;
+  printPointer(x);
+  printPointer(y);
+
+
+  cout << "----" << " Free Store " << "----" << endl;
+  int* nums = new int[2];
+  nums[0] = 6;
+  nums[1] = 7;
+
+  printPointer(nums[0]);
+  printPointer(nums[1]);
+
+  delete [] nums; //why not?
+
+  return 0;
 }
