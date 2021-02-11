16aa108907a44c9a4e9a481f348a25dd6f5a2a7e
RootCellar <RootCellar9877@GMail.com>
2021-02-10 17:52:35 -0900

    setup program

---
+++ HW/HW2/List/Test.cpp
@@ -0,0 +1,9 @@
+/*
+ * Darian Marvel
+ * 2/10/2021
+ * Test.cpp
+ * Writing a basic class, testing with list
+*/
+
+#include "catch.hpp"
+#include <list>
---
+++ HW/HW2/List/main.cpp
@@ -0,0 +1,9 @@
+/*
+ * Darian Marvel
+ * 2/10/2021
+ * main.cpp
+ * Simple placeholder for catch to write main method, and not have to be recompiled every time
+*/
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
