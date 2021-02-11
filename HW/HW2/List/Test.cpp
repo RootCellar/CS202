d73b6f92c29958773fc2b51b37ea62e3949a0313
RootCellar <RootCellar9877@GMail.com>
2021-02-10 18:07:26 -0900

    Implement class Mob

--- HW/HW2/List/Test.cpp
+++ HW/HW2/List/Test.cpp
@@ -7,3 +7,34 @@
 
 #include "catch.hpp"
 #include <list>
+
+
+class Mob {
+
+public:
+
+  static int IDpoint;
+
+  int ID;
+  int level;
+  int hp;
+  int damage;
+  int defense;
+  int speed;
+
+  Mob(): level(1), hp(100), damage(30), defense(30), speed(10) {
+    ID = IDpoint;
+    IDpoint++;
+  }
+
+};
+
+bool operator==(const Mob& a, const Mob& b) {
+  return a.ID == b.ID;
+}
+
+int Mob::IDpoint = 0;
+
+TEST_CASE("List Tests") {
+
+};
