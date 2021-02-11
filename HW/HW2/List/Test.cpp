ed0b40295ef2e1accd6b5b6daed65ff9a459fb44
RootCellar <RootCellar9877@GMail.com>
2021-02-10 19:21:19 -0900

    test stack, find

--- HW/HW2/List/Test.cpp
+++ HW/HW2/List/Test.cpp
@@ -8,9 +8,13 @@
 #include "catch.hpp"
 #include <list>
 #include <algorithm>
+#include <iostream>
 
 using std::list;
 using std::find;
+
+using std::cout;
+using std::endl;
 
 class Mob {
 
@@ -40,7 +44,23 @@ bool operator!=(const Mob& a, const Mob& b) {
   return a.ID != b.ID;
 }
 
+bool operator>(const Mob& a, const Mob& b) {
+  return a.ID > b.ID;
+}
+
+bool operator<(const Mob& a, const Mob& b) {
+  return a.ID < b.ID;
+}
+
 int Mob::IDpoint = 0;
+
+void print(list<Mob> mobs) {
+  for(auto i = mobs.begin(); i != mobs.end(); i++) {
+    Mob current = (*i);
+
+    cout << current.ID << endl;
+  }
+}
 
 TEST_CASE("List Tests") {
 
@@ -52,11 +72,14 @@ TEST_CASE("List Tests") {
   //Test Queue
 
   Mob dummy1;
+  Mob dummy3; //used later for placing the right spot
   Mob dummy2;
 
   queue.push_back(dummy1);
   queue.push_back(dummy2);
   queue.push_back(test);
+
+  print(queue);
 
   auto get = queue.begin();
   get++;
@@ -64,9 +87,13 @@ TEST_CASE("List Tests") {
 
   REQUIRE( (*get) == test );
 
+  auto found = find(queue.begin(), queue.end(), dummy1);
+
+  REQUIRE( (*found) == dummy1 );
+
   queue.pop_front();
 
-  auto found = find(queue.begin(), queue.end(), dummy1);
+  found = find(queue.begin(), queue.end(), dummy1);
 
   REQUIRE( found == queue.end() );
 
@@ -74,7 +101,66 @@ TEST_CASE("List Tests") {
 
   stack.push_back(dummy1);
   stack.push_back(dummy2);
+  stack.push_back(test);
 
-  
+  get = stack.begin();
+  get++;
+  get++;
+
+  REQUIRE( (*get) == test );
+
+  found = find(stack.begin(), stack.end(), test);
+
+  REQUIRE( (*found) == test );
+
+  stack.pop_back();
+
+  found = find(stack.begin(), stack.end(), test);
+
+  REQUIRE( found == stack.end() );
+
+  //Test finding and placing
+
+  list<Mob> mobs;
+
+  mobs.push_back(test);
+  mobs.push_back(dummy1);
+  mobs.push_back(dummy2);
+
+  //search for dummy1
+  bool didFind = false;
+
+  size_t i;
+  auto it = mobs.begin();
+  for(i=0, it = mobs.begin(); i<mobs.size(); i++, it++) {
+    Mob current = *it;
+
+    if(current == dummy1) {
+      didFind = true;
+      break;
+    }
+
+  }
+
+  REQUIRE( didFind );
+
+  //place dummy3
+  bool placed = false;
+  auto end = mobs.end()--;
+  for(auto i = mobs.begin(); i != end; i++) {
+    if( (*i) < dummy3 && (*(i++)) > dummy3 ) {
+      mobs.insert(i, dummy3);
+    }
+  }
+  if(!placed) mobs.push_back(dummy3);
+
+  //dummy3 should be at spot 2
+  get = mobs.begin();
+  get++;
+  get++;
+
+  print(mobs);
+
+  REQUIRE( (*get) == dummy3 );
 
 };
