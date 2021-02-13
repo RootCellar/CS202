8ce3bec6117d0f72d5e873588aaf09228125295f
RootCellar <RootCellar9877@GMail.com>
2021-02-12 17:19:12 -0900

    implement to_lower()

--- HW/HW2/Lower/main.cpp
+++ HW/HW2/Lower/main.cpp
@@ -5,14 +5,40 @@
  * Writing a to_lower function for C style strings
 */
 
+//#define DEBUG
+
 #include <iostream>
 #include <string>
+#include <string.h>
 
 using std::cout;
 using std::endl;
 using std::string;
 
+//make "data" all lowercase
 void to_lower(char* data) {
+
+  int i = 0;
+  while(data[i] != '\0') {
+
+    #ifdef DEBUG
+    cout << i << " " << data[i] << endl;
+    #endif
+
+    if(data[i] >= 'A' && data[i] <= 'Z') {
+      char orig = data[i];
+
+      #ifdef DEBUG
+      cout << ( (char) orig ) << endl;
+      cout << ( (char) (orig + 32) ) << endl;
+      #endif
+
+      data[i] = (orig + 32);
+    }
+
+    ++i;
+
+  }
 
 }
 
@@ -24,8 +50,9 @@ int main() {
   cout << ( (int) 'Z' ) << endl;
   cout << " ---- End Helpful Numbers ---- " << endl << endl;
 
-  string s = "hI, tHErE!";
-  to_lower(s.c_str());
+  //Use strdup to copy the const string into something I can write into
+  char* s = strdup("hI, tHErE!");
+  to_lower( s );
   cout << s << endl;
 
 }
