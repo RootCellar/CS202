473a9b2cf0af8add2c7522781e9bf46e6d38019c
RootCellar <RootCellar9877@GMail.com>
2021-02-25 10:20:38 -0900

    begin implementing Money

---
+++ HW/HW3/Money/Money.hpp
@@ -0,0 +1,66 @@
+#ifndef MONEY_HPP
+#define MONEY_HPP
+
+
+class Money {
+
+  friend bool operator==(const Money &one, const Money &two);
+  friend Money operator/(const Money &one, const double two);
+  friend Money operator+(const Money &one, const Money &two);
+  friend Money operator-(const Money &one, const Money &two);
+  friend Money operator*(const Money &one, const double two);
+
+private:
+  int value;
+
+public:
+
+  Money(): value(0) {
+
+  }
+
+  Money(double d) {
+    d *= 100;
+    value = (int)(d + 0.5);
+  }
+
+  Money(int d, int c) {
+    value = d * 100;
+    value += c;
+  }
+
+  Money operator+=(const Money &two) {
+    this.value += two.value;
+    return *this;
+  }
+
+  Money operator-=(const Money &two) {
+    this.value -= two.value;
+    return *this;
+  }
+
+  Money operator/=(const double two) {
+    this.value /= two;
+    return *this;
+  }
+
+};
+
+Money operator/(const Money &one, const double two) {
+
+}
+
+Money operator+(const Money &one, const Money &two) {
+
+}
+
+Money operator-(const Money &one, const Money &two) {
+
+}
+
+Money operator*(const Money &one, const double two) {
+
+}
+
+
+#endif
