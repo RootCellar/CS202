d348bc7ddd405052f591df7f9118cf06f7cc0222
RootCellar <RootCellar9877@GMail.com>
2021-02-27 11:08:28 -0900

    finish main()

--- HW/HW3/Token/main.cpp
+++ HW/HW3/Token/main.cpp
@@ -10,9 +10,39 @@
 #include <fstream>
 
 using std::ifstream;
+using std::cout;
+using std::endl;
 
 int main(int argc, char** argv) {
   bool fileMode = false;
   string fileName = "";
   bool printing = true;
+
+  for(int i=1; i<argc; i++) {
+
+    if( string(argv[i]) == "--lineonly" ) {
+      printing = false;
+    }
+    else {
+      //must be file name
+      fileName = argv[i];
+      fileMode = true;
+    }
+
+  }
+
+  if(!fileMode) {
+    cout << "Expected a file as parameter" << endl;
+    return 0;
+  }
+
+  vector<Token> tokens;
+  ifstream is(fileName);
+
+  tokens = readLines(is);
+
+  if(printing) {
+    printTokens(cout, tokens);
+  }
+
 }
