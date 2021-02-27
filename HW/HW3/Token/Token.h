6a2369d15b1c8d64c8825ba5fcdbc8fb26f1d73f
RootCellar <RootCellar9877@GMail.com>
2021-02-27 10:44:23 -0900

    implement readLines()

--- HW/HW3/Token/Token.h
+++ HW/HW3/Token/Token.h
@@ -1,16 +1,19 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
+#include <iostream>
 #include <ostream>
 #include <istream>
 #include <string>
 #include <vector>
+#include <sstream>
 
 using std::ostream;
 using std::istream;
 using std::string;
 using std::find;
 using std::vector;
+using std::istringstream;
 
 struct Token {
   string token;
@@ -21,5 +24,40 @@ struct Token {
 vector<string> lineToTokens(const string &line);
 vector<Token> readLines(istream &is);
 void printTokens(ostream &os, const vector<Token> &tokens);
+
+vector<Token> readLines(istream &is) {
+
+  vector<Token> toRet;
+  int lineNum = 0;
+  string line;
+
+  while(true) {
+
+    is >> line;
+    if(!is) {
+      if(is.eof()) {
+        //good, we're done
+        break;
+      }
+      //bad error
+      return toRet;
+    }
+
+    //process
+    lineNum++;
+    vector<string> tokens = lineToTokens(line);
+    for(string s : tokens) {
+      Token t;
+      t.token = s;
+      t.line = lineNum;
+      t.column = line.find(t.token);
+      toRet.push_back(t);
+    }
+
+  }
+
+  return toRet;
+
+}
 
 #endif
