fbf738db439d72234919dd96f6f5c88a236238bc
RootCellar <RootCellar9877@GMail.com>
2021-04-15 10:31:02 -0800

    complete lab 17

--- Labs/Lab17/main.cpp
+++ Labs/Lab17/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::end;
+using std::begin;
+
+using std::string;
 
 using std::cout;
 using std::endl;
 
+
+
+template <typename Iterator>
+void printReversed(Iterator theBegin, Iterator theEnd){
+// ... your code here
+  if(theEnd == theBegin) {
+    return;
+  }
+  cout << (*(theEnd-1)) << " ";
+  printReversed(theBegin, theEnd-1);
+
+}
+
 int main() {
-  
+   string s("Hello World!");
+   printReversed(begin(s),end(s));
+   cout << "\n";
+   vector<int> primes{2,3,5,7,11,13,17};
+   printReversed(begin(primes),end(primes));
+   cout << "\n";
+   return 0;
 }
